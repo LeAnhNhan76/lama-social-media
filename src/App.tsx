0b567f3686999e5697bf7ac008fbc4fa95eba2d8
Nhan Le <lenhanasus4@gmail.com>
2023-05-01 19:49:52 +0700

    #client: build & styling register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
-    <Login></Login>
+    <Register></Register>
   );
 }
 
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,6 +1,27 @@
+import './register.scss';
+
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h3>Register</h3>
+          <form>
+            <input type="text" placeholder="Username"></input>
+            <input type="email" placeholder="Email"></input>
+            <input type="password" placeholder="Password"></input>
+            <input type="text" placeholder="Name"></input>
+            <button>Register</button>
+          </form>
+        </div>
+        <div className="right">
+          <h1>Lama Social.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, recusandae laudantium earum dolore ullam culpa.</p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
