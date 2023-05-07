4e9af9ac44b7ae6dd38dd30ffe3c595b11562695
Nhan Le <lenhanasus4@gmail.com>
2023-05-07 10:47:47 +0700

    #styling right bar

--- src/components/rightbar/RightBar.tsx
+++ src/components/rightbar/RightBar.tsx
@@ -1,8 +1,206 @@
 import './rightBar.scss';
+
 const RightBar = () => {
   return (
-    <div className="rightBar">RightBar</div>
+    <div className="rightBar">
+      <div className='container'>
+        <div className="item">
+          <h5>Suggestions for you</h5>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+            </div>
+            <div className="right">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+            </div>
+            <div className="right">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+            </div>
+            <div className="right">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div> 
+        </div>
+
+        <div className="item">
+          <h5>Latest activities</h5>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span> chage their cover picture</p>
+            </div>
+            <div className="right">
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span> chage their cover picture</p>
+            </div>
+            <div className="right">
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span> chage their cover picture</p>
+            </div>
+            <div className="right">
+              <span>1 min ago</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <h5>Online friends</h5>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+          <div className="item-row">
+            <div className="left">
+              <img src='https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+              />
+              <p><span>Lindsey Davidson</span></p>
+              <div className='status'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default RightBar
+export default RightBar;
